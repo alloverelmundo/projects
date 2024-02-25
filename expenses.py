1d562c0db4f5be1a9c7e230b12460ecc256e1323
Vivian Karamitros <130526369+alloverelmundo@users.noreply.github.com>
2024-02-25 18:48:02 -0500

    Create expenses.py

---
+++ expenses.py
@@ -0,0 +1,32 @@
+expenses = [10.50,8,5,15,20,5,3];
+
+#print("you spent $", sum, sep = ''); # the comma means we dont have to convert to string and automatically adds a space
+
+#or use sum function
+
+total = sum(expenses);
+print(str(total));
+
+range(7); #automatic from 0 to 6
+range(0,7,1); #start, stop, increment
+
+#adding input to the expenses calculator
+
+total1 = 0;
+expenses1 = []; 
+for i in range(7):
+    expenses1.append(float(input("enter an expense:")));
+
+total1 = sum(expenses1);
+
+print(total1);
+
+#or
+
+total2 = 0;
+expenses2 = [];
+num_expenses = int(input("enter # of expenses:"))
+for i in range(num_expenses):
+    expenses2.append(float(input("enter an expense:")))
+total = sum(expenses2);
+print('you spent$', total, sep='');
