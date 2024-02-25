32ef6e8c38b0eb1494c1511aa77ac6176b3a4ce8
Vivian Karamitros <130526369+alloverelmundo@users.noreply.github.com>
2024-02-25 18:38:31 -0500

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,12 @@
+class robotdog:
+    def __init__(self, nameval, breedval) -> None: #init method lets us initialize our robot's properties such as name or breed
+        #self is always the first parameter and refers to instance of the class being created or the current object
+        self.name = nameval
+        self.breed = breedval
+    def bark(self): #a method is a function that belongs to a class
+        print('woof')
+
+#creating an object
+mydog = robotdog('spot', 'pitbull')
+print(mydog.name)
+mydog.bark()
