5ff2f9aae32234003cf2c5637e5b94a0808f76a1
Vivian Karamitros <130526369+alloverelmundo@users.noreply.github.com>
2024-02-25 18:39:20 -0500

    Create company.py

---
+++ company.py
@@ -0,0 +1,34 @@
+from employee import employee, salaryemployee, hourlyemployee, commissionemployee;
+
+class company:
+    def __init__(self):
+        self.employees = [];
+
+    def addemployee(self, newemployee):
+        self.employees.append(newemployee);
+
+    def displayemployees(self):
+        print('current employees')
+        for i in self.employees:
+            print(i.fname, i.lname)
+        print('---------')
+    
+    def payemployees(self):
+        print('paying employees:')
+        for i in self.employees:
+            print(f'Amount: ${i.calculatepaycheck():,.2f}');
+
+def main ():
+    mycompany = company()
+
+    employee1 = salaryemployee('jan','smith',100000)
+    mycompany.addemployee(employee1)
+    employee2 = hourlyemployee('jack','stan',25, 50)
+    mycompany.addemployee(employee2)
+    employee3 = commissionemployee('jane','doe', 30000, 5, 200)
+    mycompany.addemployee(employee3)
+
+    mycompany.displayemployees();
+    mycompany.payemployees();
+
+main();
