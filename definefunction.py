92e473ed1d6be13d734cf40a7b6f0cb86cae8b35
Vivian Karamitros <130526369+alloverelmundo@users.noreply.github.com>
2024-02-25 18:40:26 -0500

    Create definefunction.py

---
+++ definefunction.py
@@ -0,0 +1,11 @@
+def greeting(name): #using the def keyword, greeting is the function name, and name is a parameter (can have 0 to infinity parameters)
+    print('Hello', name);
+
+#main program
+input_name = input('Enter your name:\n');
+greeting(input_name)
+
+def addition(a, b):
+    return a + b;
+
+print(addition(1,2))
