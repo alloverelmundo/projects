4c998873498a2b4a60ec1a163b6dc8f7441a207e
Vivian Karamitros <130526369+alloverelmundo@users.noreply.github.com>
2024-02-25 18:50:12 -0500

    Create jsonpip.py

---
+++ jsonpip.py
@@ -0,0 +1,8 @@
+import requests
+
+response = requests.get('http://api.open-notify.org/astros.json')
+json = response.json() #to decode the json from the response
+print(json)
+
+for person in json['people']:
+    print(person['name'])
