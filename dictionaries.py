19125b5775d663b63bbfb4bfca94c3d70415187e
Vivian Karamitros <130526369+alloverelmundo@users.noreply.github.com>
2024-02-25 18:40:49 -0500

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,24 @@
+acronymslist = {'lol': 'laugh out loud',
+                'idk': 'i dont know',
+                'tbh': 'to be honest'}
+
+#creating a dictionary from nothing
+
+acronymslist1 = {};
+acronymslist1['lol']='laugh out loud';
+
+#updating a value
+acronymslist1['lol'] = 'just laughing';
+
+#deleting a value
+del acronymslist1['lol'];
+
+#getting an item that's not in the dictionary
+definition = acronymslist.get('lol');
+
+if definition:
+    print(definition)
+
+sentence = 'idk' + 'what happened' + 'tbh';
+translation = acronymslist.get('idk') + ' what happened ' + acronymslist.get('tbh');
+
