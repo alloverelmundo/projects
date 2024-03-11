ad2895d8cf2d0d4e130da0d9a9c38763590b588b
Vivian Karamitros <130526369+alloverelmundo@users.noreply.github.com>
2024-03-11 18:46:40 -0400

    Update decades.py

--- decades.py
+++ decades.py
@@ -1,3 +1,5 @@
+#This code prompts the user to input their age, calculates the number of decades and remaining years based on that age, and then prints out the result.
+
 age = int(input("How old are you?\n"));
 decades = age//10; # two backslahes does whole number division
 years = age % 10; # the % symbol gets the remainder
