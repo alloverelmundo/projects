5a077b2da50ea3ddd63c91a21decc2c9bb6ab787
Vivian Karamitros <130526369+alloverelmundo@users.noreply.github.com>
2024-03-11 20:39:42 -0400

    Update jsonpip.py

--- jsonpip.py
+++ jsonpip.py
@@ -1,3 +1,5 @@
+#This code sends a request to the Open Notify API to retrieve information about astronauts currently in space, then parses the JSON response to print out the names of the astronauts.
+
 import requests
 
 response = requests.get('http://api.open-notify.org/astros.json')
